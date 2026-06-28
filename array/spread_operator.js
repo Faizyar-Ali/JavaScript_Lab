3da6e31fe4921483a676bebb197a25aeea944333
Faizyar Ali <shaikhfaizy84@gmail.com>
2026-06-28 14:02:55 +0500

    Learned Spread_Operator

---
+++ array/spread_operator.js
@@ -0,0 +1,12 @@
+const arr=[10,20,30,40]
+const name=['faizy','alis','eliot']
+
+
+//to exact copy an existed array into new array
+const arr2=[...arr]
+console.log(arr2)
+
+
+// combine two array
+const comb_arr =[...arr,...name]
+console.log(comb_arr)
